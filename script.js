3ef7c3a4bbaa9ac213890f2e634f8a4c4a11e72f
Khazeral <wandehousse@gmail.com>
2025-01-29 23:24:31 +0100

    style : format file with Prettier

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-const cases = document.getElementsByClassName("case")
-const button = document.querySelector("button")
-const container = document.getElementById("container-table")
-const dimension = 0
+const cases = document.getElementsByClassName("case");
+const button = document.querySelector("button");
+const container = document.getElementById("container-table");
+const dimension = 0;
 
-Array.from(cases).forEach(element => {
-    element.addEventListener("mouseenter", () => {element.style.backgroundColor = 'red'})
+Array.from(cases).forEach((element) => {
+  element.addEventListener("mouseenter", () => {
+    element.style.backgroundColor = "red";
+  });
 });
 
-button.addEventListener("click", () =>{
-    let dimensionInput = Number(prompt("Entrer la dimension de la grille (entre 1 et 100) : "))
-    if(dimensionInput === NaN){
-        alert("Ceci n'est pas un nombre")
-        dimensionInput = Number(prompt("Entrer la dimension de la grille : "))
-    }
-    removePreviousCanva()
-    buildCanvas(dimensionInput)
-})
+button.addEventListener("click", () => {
+  let dimensionInput = Number(
+    prompt("Entrer la dimension de la grille (entre 1 et 100) : ")
+  );
+  if (dimensionInput === NaN) {
+    alert("Ceci n'est pas un nombre");
+    dimensionInput = Number(prompt("Entrer la dimension de la grille : "));
+  }
+  removePreviousCanva();
+  buildCanvas(dimensionInput);
+});
 const removePreviousCanva = () => {
-    while(container.firstChild) { 
-        container.removeChild(container.firstChild); 
-    } 
-}
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
 
 const buildCanvas = (dimension) => {
-    for(let i =0 ; i< dimension; i++){
-        const line = document.createElement("div")
-        line.className = "line"
-        for(let j =0 ; j< dimension; j++){
-            const square = document.createElement("div")
-            square.className = "case"
-            square.addEventListener("mouseenter", () => {square.style.backgroundColor = 'red'})
-            line.appendChild(square)
-        }
-        container.appendChild(line)
+  for (let i = 0; i < dimension; i++) {
+    const line = document.createElement("div");
+    line.className = "line";
+    for (let j = 0; j < dimension; j++) {
+      const square = document.createElement("div");
+      square.className = "case";
+      square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "red";
+      });
+      line.appendChild(square);
     }
-}
+    container.appendChild(line);
+  }
+};
